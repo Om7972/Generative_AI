dc611636041e640b063d5a6d990874f2c849598a
Om7972 <odhumkekar@gmail.com>
2026-04-15 19:58:14 +0530

    feat: add AdherenceChart and SimulationGraph components for visualizing health metrics

--- client/src/components/ui/AdherenceChart.jsx
+++ client/src/components/ui/AdherenceChart.jsx
@@ -51,8 +51,8 @@ const AdherenceChart = ({ medications = [], title = 'Medication Adherence' }) =>
         </div>
       </div>
 
-      <div className="h-48">
-        <ResponsiveContainer width="100%" height="100%">
+      <div className="h-48 w-full">
+        <ResponsiveContainer width="100%" height="100%" minWidth={0}>
           <AreaChart data={data} margin={{ top: 5, right: 5, bottom: 0, left: -20 }}>
             <defs>
               <linearGradient id="adherenceGradient" x1="0" y1="0" x2="0" y2="1">
